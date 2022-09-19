9005906796d027789b56c9a76fccc29949db5cc8
신용준 <63823237+shinyj1991@users.noreply.github.com>
2022-09-19 17:19:45 +0900

    Updates

---
+++ component---src-pages-demo-test-jsx-f2ba3128338863bf1934.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[9],{F249:function(e,n,t){"use strict";t.r(n);var a=t("q1tI"),p=t.n(a),l=t("vOnD").default.div.withConfig({displayName:"test__Wrapper",componentId:"sc-1nr39pt-0"})(["padding:30px;"]);n.default=function(){return p.a.createElement(l,null,p.a.createElement("div",null,p.a.createElement("p",null,"Demo Test")))}}}]);
+//# sourceMappingURL=component---src-pages-demo-test-jsx-f2ba3128338863bf1934.js.map
