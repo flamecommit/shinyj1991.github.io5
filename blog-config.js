54fb8fb57c2327176857395b5e6434babe333228
신용준 <63823237+shinyj1991@users.noreply.github.com>
2022-09-19 14:54:18 +0900

    start: gatsby markdown blog starter

---
+++ blog-config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  title: "simplizm",
+  description: "tech blog",
+  author: "Shinyj1991",
+  siteUrl: "https://simplizm.xyz",
+  links: {
+    github: "https://github.com/shinyj1991",
+    // linkedIn: "https://linkedin.com",
+    // facebook: "https://www.facebook.com",
+    // instagram: "https://www.instagram.com",
+    email: "mailto:shinyj1991@gmail.com",
+  },
+  utterances: {
+    repo: "shinyj1991/simplizm",
+    type: "pathname",
+  },
+}
