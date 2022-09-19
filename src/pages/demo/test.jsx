3ff96d7830f66b76e67dcede0c519018cf34a8b9
신용준 <63823237+shinyj1991@users.noreply.github.com>
2022-09-19 17:22:01 +0900

    feat: 한글포스트 테스트

---
+++ src/pages/demo/test.jsx
@@ -0,0 +1,18 @@
+import React from "react"
+import styled from "styled-components"
+
+const Wrapper = styled.div`
+  padding: 30px;
+`
+
+const PageDemoTest = () => {
+  return (
+    <Wrapper>
+      <div>
+        <p>Demo Test</p>
+      </div>
+    </Wrapper>
+  )
+}
+
+export default PageDemoTest
--- src/reducers/theme.js
+++ src/reducers/theme.js
@@ -1,5 +1,5 @@
 export const initialState = {
-  theme: "dark",
+  theme: "light",
 }
 
 // action type 정의
