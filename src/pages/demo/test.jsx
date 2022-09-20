b626fd5b9981d659414093a4c4725ce1133e0a35
신용준 <63823237+shinyj1991@users.noreply.github.com>
2022-09-20 11:37:48 +0900

    feat: demo page test

--- src/pages/demo/test.jsx
+++ src/pages/demo/test.jsx
@@ -1,17 +1,22 @@
 import React from "react"
 import styled from "styled-components"
 
+import Layout from "components/Layout"
+
 const Wrapper = styled.div`
   padding: 30px;
+  color: #fff;
 `
 
 const PageDemoTest = () => {
   return (
-    <Wrapper>
-      <div>
-        <p>Demo Test</p>
-      </div>
-    </Wrapper>
+    <Layout>
+      <Wrapper>
+        <div>
+          <p>Demo Test</p>
+        </div>
+      </Wrapper>
+    </Layout>
   )
 }
 
