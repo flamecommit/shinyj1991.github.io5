c9033bc64969bfd4116594ff9dac868b0ab9f6b4
신용준 <63823237+shinyj1991@users.noreply.github.com>
2022-09-29 13:50:42 +0900

    feat: about and history

---
+++ src/assets/data/history.js
@@ -0,0 +1,66 @@
+export default [
+  {
+    period: '2022. 2 ~ 재직중',
+    company: '위메이드',
+    team: '웹개발팀',
+    position: '과장',
+    duty: '웹서비스 구축 및 운영',
+    skill: [
+      'html',
+      'scss',
+      'javascript',
+      'vue'
+    ]
+  },
+  {
+    period: '2019. 2 ~ 2022. 2',
+    company: '뉴본홀딩스',
+    team: 'IT사업부',
+    position: '과장',
+    duty: '웹서비스 구축 및 운영',
+    skill: [
+      'html',
+      'scss',
+      'javascript',
+      'jquery',
+      'vue'
+    ]
+  },
+  {
+    period: '2015. 4 ~ 2018. 8',
+    company: '뉴리버',
+    team: '웹퍼블리싱팀',
+    position: '팀장',
+    duty: '웹서비스 구축 및 운영',
+    skill: [
+      'html',
+      'css',
+      'javascript',
+      'jquery'
+    ]
+  },
+  {
+    period: '2014. 2 ~ 2015. 3',
+    company: '메가코딩',
+    team: '웹퍼블리싱팀',
+    position: '사원',
+    duty: '웹서비스 구축 및 운영',
+    skill: [
+      'html',
+      'css',
+      'javascript',
+      'jquery'
+    ]
+  },
+  {
+    period: '2013. 9 ~ 2014. 1',
+    company: 'WISE C&S',
+    team: '컨텐츠UI팀',
+    position: '사원',
+    duty: '구인공고 테이블 코딩',
+    skill: [
+      'html',
+      'css'
+    ]
+  },
+]
--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -1,12 +1,45 @@
 import React from "react"
 import styled from "styled-components"
 import SEO from "components/SEO"
+import { Link } from "gatsby"
+import _ from "lodash"
 
 import Layout from "components/Layout"
+import history from "assets/data/history"
 
 import { title, description, siteUrl } from "../../blog-config"
 
 const AboutWrapper = styled.div`
+  display: grid;
+  row-gap: 50px;
+  line-height: 1.5;
+  color: ${props => props.theme.colors.text};
+
+  h1 {
+    font-weight: 700;
+    font-size: 32px;
+  }
+  section {
+    h2 {
+      font-weight: 700;
+      font-size: 24px;
+      margin-bottom: 12px;
+    }
+    a {
+      color: #71ac42;
+    }
+    .history {
+      display: grid;
+
+      li {
+        padding: 15px 0;
+
+        &:not(:last-child) {
+          border-bottom: 1px solid ${props => props.theme.colors.divider};
+        }
+      }
+    }
+  }
 `
 
 const AboutPage = () => {
@@ -16,6 +49,38 @@ const AboutPage = () => {
 
       <AboutWrapper>
         <h1>About YJ.</h1>
+        <section>
+          <h2>Greeting.</h2>
+          <p>
+            안녕하세요.<br />
+            UI개발자 신용준입니다.
+          </p>
+        </section>
+        <section>
+          <h2>History.</h2>
+          <ol className="history">
+            {history.map(c => 
+              <li key={c.company}>
+                <div className="period">{c.period}</div>
+                <div className="name">{c.company} / {c.team} / {c.position}</div>
+                <div className="duty">{c.duty}</div>
+                <div className="skill">{_.join(c.skill, ', ')}</div>
+              </li>
+            )}
+          </ol>
+        </section>
+        <section>
+          <h2>Project.</h2>
+          <a href="https://simplizm.notion.site/Project-ab5993b7b6a9403088848359cd1f0b48" target="_blank" rel="noreferrer">move to project</a>
+        </section>
+        <section>
+          <h2>Github.</h2>
+          <a href="#self">move to github</a>
+        </section>
+        <section>
+          <h2>Blog.</h2>
+          <Link to="/">move to blog</Link>
+        </section>
       </AboutWrapper>
     </Layout>
   )
