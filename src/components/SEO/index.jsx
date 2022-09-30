eb3b75b801b35e6f2581f90650af9a7f668fd309
신용준 <63823237+shinyj1991@users.noreply.github.com>
2022-09-30 16:49:18 +0900

    modify: open graph, twitter

--- src/components/SEO/index.jsx
+++ src/components/SEO/index.jsx
@@ -1,16 +1,26 @@
 import React from "react"
 import { Helmet } from "react-helmet"
-import { siteUrl } from "/blog-config"
+import { siteUrl, author } from "/blog-config"
 
 const SEO = ({ title, description, url }) => {
   return (
     <Helmet>
       <title>{title}</title>
+      <meta name="author" content={author} />
+      <meta name="description" content={description} />
+
+      {/* Open graph */}
       <meta property="og:url" content={url} />
+      <meta property="og:type" content="article" />
       <meta property="og:title" content={title} />
+      <meta property="og:description" content={description} />
       <meta property="og:image" content={`${siteUrl}/og-image.png`} />
-      {description && <meta name="description" content={description} />}
-      {description && <meta property="og:description" content={description} />}
+
+      {/* Twitter */}
+      <meta name="twitter:card" content="summary" />
+      <meta name="twitter:title" content={title} />
+      <meta name="twitter:description" content={description} />
+      <meta name="twitter:image" content={`${siteUrl}/og-image.png`} />
     </Helmet>
   )
 }
